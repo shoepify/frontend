890a150ac3abb2d91b59c53bccd8fee30768024b
cannarin <narincan@sabanciuniv.edu>
2024-11-20 16:20:30 +0300

    bazi dosyalar temizlendi

--- src/pages/ProductManager.js
+++
@@ -1,8 +0,0 @@
-// src/pages/ProductManagerPage.js
-import React from 'react';
-
-const ProductManagerPage = () => {
-    return <h1>Welcome, Product Manager!</h1>;
-};
-
-export default ProductManagerPage;
